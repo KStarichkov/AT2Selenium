b558d59458b8cc3d1c8a65e778754b7819f311a9
KStarichkov <ghilmorkosta@gmail.com>
2026-06-02 00:23:27 +0400

    New selenium

---
+++ src/test/java/ru/course/at/SeleniumTests.java
@@ -0,0 +1,24 @@
+package ru.course.at;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class SeleniumTests {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+    @AfterEach
+    public void tearDown() { driver.quit(); }
+
+    @Test
+    public void openBingTest() { driver.get("https://www.bing.com"); }
+}
