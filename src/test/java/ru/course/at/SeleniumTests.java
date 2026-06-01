09660c13f6863ecb31fd423bea731969d3b86e1e
KStarichkov <ghilmorkosta@gmail.com>
2026-06-02 00:58:01 +0400

    New selenium

--- src/test/java/ru/course/at/SeleniumTests.java
+++ src/test/java/ru/course/at/SeleniumTests.java
@@ -3,9 +3,13 @@ package ru.course.at;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SeleniumTests {
     private WebDriver driver;
@@ -14,11 +18,22 @@ public class SeleniumTests {
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bing.com");
     }
     @AfterEach
     public void tearDown() { driver.quit(); }
 
     @Test
-    public void openBingTest() { driver.get("https://www.bing.com"); }
+    public void search() {
+        String input = "Selenium";
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys(input);
+        searchField.submit();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
+        assertEquals(input, searchPageField.getAttribute("value"));
+
+        System.out.println("Заголовок страницы: " + driver.getTitle());
+    }
 }
